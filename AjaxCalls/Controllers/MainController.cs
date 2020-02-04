35f96cbcf1cfcef8b290d6a5f137d8a9bf49cf4e
oscarsolerfollana <31385047+oscarsolerfollana@users.noreply.github.com>
2020-02-04 07:46:56 +0100

    new add

---
+++ AjaxCalls/Controllers/MainController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AjaxCalls.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class MainController : Controller
+    {
+        List<string> names = new List<string>
+        {
+            "Ana", "Alberto", "Alba", "Luís", "Laura", "Luisa"
+        };
+        [Route("index")]
+        [HttpGet]
+        public async Task<ActionResult> Index()
+        {
+            return View("~/Views/Main.cshtml");
+        }
+
+        [HttpPost("getnames")]
+        public JsonResult GetNames([FromBody] Pattern pattern)
+        {
+            lock("AjaxCall")
+            {
+                System.IO.File.AppendAllText(@"c:\prueba\log.txt", pattern.text + Environment.NewLine);
+            }
+            if(pattern.text=="")
+            {
+                return Json(names);
+            }
+            return Json(names.Where(str => str.ToUpper().StartsWith(pattern.text.ToUpper())));
+        }
+    }
+
+    public class Pattern
+    {
+        public string text { get; set; }
+    }
+}
---
+++ AjaxCalls/wwwroot/scripts/AjaxScript.js
@@ -0,0 +1,23 @@
+﻿function getNames() {
+    $("#log").append(" " + $("#textBox").val());
+    var parameters = {
+        "text": $("#textBox").val()
+    };
+    $.ajax({
+        type: "POST",
+        url: "/main/getnames",
+        data: JSON.stringify(parameters),
+        contentType: 'application/json',
+        statusCode: {
+            200: function (response) {
+                $("#label").html("");
+                for (var i = 0; i < response.length; i++) {
+                    $("#label").append(response[i] + "<br>");
+                }
+            }
+        },
+        error: function () {
+            alert("Error!")
+        }
+    });
+}
